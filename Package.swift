9bb1900993a1cacf8182c959180b9b148bc2806b
narumij <narumij@gmail.com>
2025-01-09 19:34:58 +0900

    re-initialize package

--- Package.swift
+++ Package.swift
@@ -2,23 +2,50 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "Memoize",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Memoize",
-            targets: ["Memoize"]),
+            targets: ["Memoize"]
+        ),
+        .executable(
+            name: "MemoizeClient",
+            targets: ["MemoizeClient"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Memoize"),
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "MemoizeMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(name: "Memoize", dependencies: ["MemoizeMacros"]),
+
+        // A client of the library, which is able to use the macro in its own code.
+        .executableTarget(name: "MemoizeClient", dependencies: ["Memoize"]),
+
+        // A test target used to develop the macro implementation.
         .testTarget(
             name: "MemoizeTests",
-            dependencies: ["Memoize"]
+            dependencies: [
+                "MemoizeMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
         ),
     ]
 )
--- Tests/MemoizeTests/MemoizeTests.swift
+++ Tests/MemoizeTests/MemoizeTests.swift
@@ -1,6 +1,48 @@
-import Testing
-@testable import Memoize
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import XCTest
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+// Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
+#if canImport(MemoizeMacros)
+import MemoizeMacros
+
+let testMacros: [String: Macro.Type] = [
+    "stringify": StringifyMacro.self,
+]
+#endif
+
+final class MemoizeTests: XCTestCase {
+    func testMacro() throws {
+        #if canImport(MemoizeMacros)
+        assertMacroExpansion(
+            """
+            #stringify(a + b)
+            """,
+            expandedSource: """
+            (a + b, "a + b")
+            """,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+
+    func testMacroWithStringLiteral() throws {
+        #if canImport(MemoizeMacros)
+        assertMacroExpansion(
+            #"""
+            #stringify("Hello, \(name)")
+            """#,
+            expandedSource: #"""
+            ("Hello, \(name)", #""Hello, \(name)""#)
+            """#,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
 }
