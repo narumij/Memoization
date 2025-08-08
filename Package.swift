c16905b8a46fb78308af6ae68b50af8fdf22aab8
narumij <narumij@gmail.com>
2025-08-08 21:37:26 +0900

    reproduce for feedback assistant

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
     .package(
       url: "https://github.com/narumij/swift-ac-collections",
-      from: "0.1.30"),
+      revision: "aada19cc34ae40fba6be30dbdef145d5905b7ab0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
